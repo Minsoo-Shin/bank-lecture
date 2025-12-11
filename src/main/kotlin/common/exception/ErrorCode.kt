404bf2fa67dddf0de888946721e80babf34b8b6d
Minsoo-Shin <alstn5038@gmail.com>
2025-12-11 21:14:17 +0900

    feat: logging AOP

--- src/main/kotlin/common/exception/ErrorCode.kt
+++ src/main/kotlin/common/exception/ErrorCode.kt
@@ -10,9 +10,10 @@ enum class ErrorCode(
     override var message: String
 ) : CodeInterface {
     AUTH_CONFIG_NOT_FOUND(-100, "auth config not found"),
-    FAILED_TO_CALL_CLIENT(-100, "failed to call client"),
-    CALL_RESULT_BODY_NULL(-100, "body is null"),
-    PROVIDER_NOT_FOUND(-100, "provider not found"),
-    TOKEN_IS_INVALID(-100, "token is invalid"),
-    TOKEN_IS_EXPIRED(-100, "token is expired")
+    FAILED_TO_CALL_CLIENT(-101, "failed to call client"),
+    CALL_RESULT_BODY_NULL(-102, "body is null"),
+    PROVIDER_NOT_FOUND(-103, "provider not found"),
+    TOKEN_IS_INVALID(-104, "token is invalid"),
+    TOKEN_IS_EXPIRED(-105, "token is expired"),
+    FAILED_TO_INVOKE_IN_LOGGER(-106, "failed to invoke in logger"),
 }
