039632eb01921a1d8e4f7798a7ca3b1e1e994318
Minsoo-Shin <alstn5038@gmail.com>
2025-12-11 15:29:17 +0900

    feat: getToken 구현 (github, google)

--- src/main/kotlin/domains/auth/service/GithubAuthService.kt
+++ src/main/kotlin/domains/auth/service/GithubAuthService.kt
@@ -2,24 +2,44 @@ package com.example.domains.auth.service
 
 import com.example.common.exception.CustomException
 import com.example.common.exception.ErrorCode
+import com.example.common.httpClient.CallClient
 import com.example.config.OAuth2Config
 import com.example.interfaces.OAuth2TokenResponse
 import com.example.interfaces.OAuth2UserResponse
 import com.example.interfaces.OAuthServiceInterface
+import okhttp3.FormBody
 import org.springframework.stereotype.Service
+import kotlin.collections.mapOf
 
 private const val key = "github"
 
 @Service(key)
 class GithubAuthService(
-    private val config: OAuth2Config
+    private val config: OAuth2Config,
+    private val httpClient: CallClient,
 ) : OAuthServiceInterface {
     private val oAuthInfo = config.providers[key] ?: throw CustomException(ErrorCode.AUTH_CONFIG_NOT_FOUND, key)
+    private val tokenURL = "https://oauth2.googleapis.com/token"
+    private val userInfoURL = "https://www.googleapis.com/oauth2/v2/userinfo"
 
     override val providerName: String = key
 
     override fun getToken(code: String): OAuth2TokenResponse {
-        TODO("Not yet implemented")
+        val body = FormBody.Builder()
+            .add("code", code)
+            .add("client_id", oAuthInfo.clientId)
+            .add("client_secret", oAuthInfo.clientSecret)
+            .add("redirect_uri", oAuthInfo.redirectUri)
+            .add("grant_type", "authorization_code")
+            .build()
+
+        val headers = mapOf("Accespt" to "application/json")
+        httpClient.POST(tokenURL, headers, body)
+
+        // jsonString -> json 처리
+        
+
+        TODO()
     }
 
     override fun getUserInfo(accessToken: String): OAuth2UserResponse {
--- src/main/kotlin/domains/auth/service/GoogleAuthService.kt
+++ src/main/kotlin/domains/auth/service/GoogleAuthService.kt
@@ -2,23 +2,41 @@ package com.example.domains.auth.service
 
 import com.example.common.exception.CustomException
 import com.example.common.exception.ErrorCode
+import com.example.common.httpClient.CallClient
 import com.example.config.OAuth2Config
 import com.example.interfaces.OAuth2TokenResponse
 import com.example.interfaces.OAuth2UserResponse
 import com.example.interfaces.OAuthServiceInterface
+import okhttp3.FormBody
 import org.springframework.stereotype.Service
 
 private const val key = "google"
 
 @Service(key)
 class GoogleAuthService(
-    private val config: OAuth2Config
+    private val config: OAuth2Config,
+    private val httpClient: CallClient
 ) : OAuthServiceInterface {
     private val oAuthInfo = config.providers[key] ?: throw CustomException(ErrorCode.AUTH_CONFIG_NOT_FOUND, key)
+    private val tokenURL = "https://github.com/login/oauth/access_token"
+    private val userInfoURL = "https://api.github.com/user"
 
     override val providerName: String = key
 
     override fun getToken(code: String): OAuth2TokenResponse {
+        val body = FormBody.Builder()
+            .add("code", code)
+            .add("client_id", oAuthInfo.clientId)
+            .add("client_secret", oAuthInfo.clientSecret)
+            .add("redirect_uri", oAuthInfo.redirectUri)
+            .add("grant_type", "authorization_code")
+            .build()
+
+        val headers = mapOf("Accespt" to "application/json")
+        httpClient.POST(tokenURL, headers, body)
+
+        // jsonString -> json 처리
+
         TODO("Not yet implemented")
     }
 
