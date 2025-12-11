81b53959917a6d959be26ddf1e18a30024cb112e
Minsoo-Shin <alstn5038@gmail.com>
2025-12-11 21:14:17 +0900

    feat: logging AOP

--- src/main/kotlin/domains/auth/service/AuthService.kt
+++ src/main/kotlin/domains/auth/service/AuthService.kt
@@ -3,26 +3,32 @@ package com.example.domains.auth.service
 import com.example.common.exception.CustomException
 import com.example.common.exception.ErrorCode
 import com.example.common.jwt.JwtProvider
+import com.example.common.logging.Logging
 import com.example.interfaces.OAuthServiceInterface
+import org.slf4j.Logger
 import org.springframework.stereotype.Service
 
 @Service
 class AuthService(
     private val oAuth2Service: Map<String, OAuthServiceInterface>,
     private val jwtProvider: JwtProvider,
+    private val logger: Logger = Logging.getLogger(AuthService::class.java),
 ) {
-    fun handleAuth(state: String, code: String): String {
+    fun handleAuth(state: String, code: String): String = Logging.logFor(logger) { log ->
         val provider = state.lowercase()
+        log["provider"] = provider
 
         val callService = oAuth2Service[provider] ?: throw CustomException(ErrorCode.PROVIDER_NOT_FOUND, provider)
 
         val accessToken = callService.getToken(code)
         val userInfo = callService.getUserInfo(accessToken = accessToken.accessToken)
-        return jwtProvider.createToken(
+
+        jwtProvider.createToken(
             platform = provider,
             email = userInfo.email,
             name = userInfo.name,
             id = userInfo.id
         )
+        TODO()
     }
 }
