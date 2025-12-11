08701e1db0be603e5c6dbb2e0cea9eeffd765566
Minsoo-Shin <alstn5038@gmail.com>
2025-12-11 15:13:44 +0900

    feat: httpClient 추가

--- src/main/kotlin/common/exception/ErrorCode.kt
+++ src/main/kotlin/common/exception/ErrorCode.kt
@@ -9,5 +9,8 @@ enum class ErrorCode(
     override val code: Int,
     override var message: String
 ) : CodeInterface {
-    AUTH_CONFIG_NOT_FOUND(-100, "auth config not found")
+    AUTH_CONFIG_NOT_FOUND(-100, "auth config not found"),
+    FAILED_TO_CALL_CLIENT(-100, "failed to call client"),
+    CALL_RESULT_BODY_NULL(-100, "body is null")
+
 }
