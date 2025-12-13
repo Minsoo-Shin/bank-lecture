607f57512a8789c18b430719d83c17d4733c2f30
Minsoo-Shin <alstn5038@gmail.com>
2025-12-13 13:36:13 +0900

    feat: Account isDeletedAt 추가

--- src/main/kotlin/types/entity/Account.kt
+++ src/main/kotlin/types/entity/Account.kt
@@ -24,17 +24,20 @@ data class Account(
     val user: User,
 
     @Column(name = "balance", nullable = false, precision = 15, scale = 2)
-    var balance : BigDecimal = BigDecimal.ZERO,
+    var balance: BigDecimal = BigDecimal.ZERO,
 
     @Column(name = "account_number", length = 100, nullable = false, unique = true)
-    val accountNumber : String,
+    val accountNumber: String,
 
     @Column(name = "is_deleted", nullable = false)
-    val isDeleted : Boolean = false,
+    val isDeleted: Boolean = false,
 
     @Column(name = "created_at", nullable = false, updatable = false)
     val createdAt: LocalDateTime = LocalDateTime.now(),
 
     @Column(name = "updated_at", nullable = false)
     var updatedAt: LocalDateTime = LocalDateTime.now(),
+
+    @Column(name = "deleted_at")
+    var deletedAt: LocalDateTime? = null,
 )
