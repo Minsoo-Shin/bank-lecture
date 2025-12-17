0c06acf02a451ad0827d9f07906703c35c693f0c
Minsoo-Shin <alstn5038@gmail.com>
2025-12-17 14:11:46 +0900

    fix: sendMessage 실패 핸들러 처리 수정

--- src/main/kotlin/common/message/KafkaProducer.kt
+++ src/main/kotlin/common/message/KafkaProducer.kt
@@ -1,14 +1,10 @@
 package com.example.common.message
 
-import com.example.common.exception.CustomException
-import com.example.common.exception.ErrorCode
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.kafka.support.SendResult
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime
-import java.util.concurrent.CompletableFuture
 
 @Component
 class KafkaProducer(
@@ -16,14 +12,13 @@ class KafkaProducer(
     private val log: Logger = LoggerFactory.getLogger(KafkaProducer::class.java)
 ) {
     fun sendMessage(topic: String, message: String) {
-        val future: CompletableFuture<SendResult<String, String>> = kafkaTemplate.send(topic, message)
-        future.whenComplete { metadata, ex ->
+        kafkaTemplate.send(topic, message).whenComplete { metadata, ex ->
             if (ex == null) {
                 // hanlde success
-                log.info("메시지 발행 성공 - topic: $topic - time: ${LocalDateTime.now()}")
+                log.info("메시지 발행 성공 - topic: ${metadata.recordMetadata.topic()} - time: ${LocalDateTime.now()}")
             } else {
                 // handle failure
-                throw CustomException(ErrorCode.FAILED_TO_SEND_MESSAGE, topic)
+                log.error(ex.stackTraceToString())
             }
         }
     }
