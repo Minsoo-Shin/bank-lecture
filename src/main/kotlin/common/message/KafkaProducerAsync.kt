fb58fe77f1c09c3d89ecb4d8ae027400d017f663
Minsoo-Shin <alstn5038@gmail.com>
2025-12-17 17:27:15 +0900

    예외 처리 로깅 및 기타 처리

--- src/main/kotlin/common/message/KafkaProducerAsync.kt
+++ src/main/kotlin/common/message/KafkaProducerAsync.kt
@@ -5,7 +5,6 @@ import org.slf4j.Logger
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.support.SendResult
 import org.springframework.stereotype.Component
-import java.time.LocalDateTime
 import java.util.concurrent.CompletableFuture
 
 @Component
@@ -16,11 +15,16 @@ class KafkaProducerAsync(
     fun sendMessageAsync(topic: String, message: String): CompletableFuture<SendResult<String, String>> {
         return kafkaTemplate.send(topic, message).whenComplete { metadata, ex ->
             if (ex == null) {
-                // hanlde success
-                log.info("메시지 발행 성공 - topic: ${metadata.recordMetadata.topic()} - time: ${LocalDateTime.now()}")
+                // handle success
+                log.info(
+                    "메시지 발행 성공 - topic: {}, partition: {}, offset: {}",
+                    metadata.recordMetadata.topic(),
+                    metadata.recordMetadata.partition(),
+                    metadata.recordMetadata.offset()
+                )
             } else {
                 // handle failure (TODO: 리트라이/DLT 추가)
-                log.error(ex.stackTraceToString())
+                log.error("메시지 발행 실패 - topic: {}", topic, ex)
             }
         }
     }
