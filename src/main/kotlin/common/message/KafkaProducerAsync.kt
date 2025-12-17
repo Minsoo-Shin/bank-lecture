e919e4a321558ece00ec733a96913c10bb098aba
Minsoo-Shin <alstn5038@gmail.com>
2025-12-17 17:19:35 +0900

    feat: kafka producer async future 반환

--- src/main/kotlin/common/message/KafkaProducerAsync.kt
+++ src/main/kotlin/common/message/KafkaProducerAsync.kt
@@ -1,18 +1,20 @@
 package com.example.common.message
 
+import com.example.common.logging.Logging
 import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.SendResult
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime
+import java.util.concurrent.CompletableFuture
 
 @Component
 class KafkaProducerAsync(
     private val kafkaTemplate: KafkaTemplate<String, String>,
-    private val log: Logger = LoggerFactory.getLogger(KafkaProducerAsync::class.java)
+    private val log: Logger = Logging.getLogger(KafkaProducerAsync::class.java)
 ) {
-    fun sendMessageAsync(topic: String, message: String) {
-        kafkaTemplate.send(topic, message).whenComplete { metadata, ex ->
+    fun sendMessageAsync(topic: String, message: String): CompletableFuture<SendResult<String, String>> {
+        return kafkaTemplate.send(topic, message).whenComplete { metadata, ex ->
             if (ex == null) {
                 // hanlde success
                 log.info("메시지 발행 성공 - topic: ${metadata.recordMetadata.topic()} - time: ${LocalDateTime.now()}")
